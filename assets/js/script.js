37683ab86ee33d6e077f76cd6ca39c57edc4b35f
Nick Bicknell <nicholasbicknell@gmail.com>
2023-07-03 19:06:58 -0500

    Added function to save info to local storage on save button click, loaded the local storage saved items back onto the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,17 +10,35 @@ currentTime.text(timeTracker);
 var currentHour = dayjs().format('HH');
 $(".time-block").each(function () {
   var timeBlock = $(this).attr("id").split("-")[1];
+  console.log("currentHour " + currentHour);
 
-  if (currentHour == timeBlock) {
-    $(this).addClass('present');
+  if (currentHour === timeBlock) {
+    $(this).addClass("present");
   } else if (currentHour < timeBlock) {
-    $(this).removeClass('present');
     $(this).addClass('future');
   } else if (currentHour > timeBlock) {
-    $(this).removeClass('present');
     $(this).addClass('past');
   }
 });
+
+$(saveBtn).on("click", function(event) {
+  event.preventDefault();
+  var desc = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+  
+  console.log(desc + " " + time);
+  localStorage.setItem(time, desc);
+});
+
+$("#hour-09 .description").val(localStorage.getItem("hour-09"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
